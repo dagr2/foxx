7250a9a93f95d0bf174cd0e2c287a26abce8c1cb
Daniel <dagr_spam@freenet.de>
2020-06-18 08:04:02 +0200

    ...

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ const collectionName = module.context.collectionName("log");
 
 module.context.use(router);
 
-// Hello World route
-router.get('/hello-world',(req,res)=>{
-	res.send('Hello WORLD');
-})
-.response(['text/plain'], 'A generic greeting')
-.summary('Generic greeting')
-.description('prints a generic greeting');
-
-// Hello World route
 router.get('/show-log',(req,res)=>{
 	var { query } = require("@arangodb");
 	var max = 13;
@@ -28,37 +19,28 @@ router.get('/show-log',(req,res)=>{
 	res.json(log);
 });
 
-// Hello World route
-router.put('/write-log/:msg',(req,res)=>{
+router.put('/write-log',(req,res)=>{
 	var { query } = require("@arangodb");
 	var max = 13;
 	var log = query`
 	  INSERT 
 	  {
 	  	_key: ${new Date()},
-	  	msg: ${req.pathParams.msg}
+	  	msg: ${req.body}
 	  } 
 	  INTO log
 	`.toArray();
 	res.json(log);
 });
 
-// Hello World route
 router.get('/log-view',(req,res)=>{
 	var { query } = require("@arangodb");
 	var max = 13;
 	var logs = query`
 	  FOR e in log return e	`.toArray();
 	var table=`<table>${logs.map(e=>'<tr><td>'+e._key+'</td><td>'+e.msg+'</td></tr>')}</table>`;
+	res.set('Content-Type', 'text/html');
 	res.send(`<html><body>${table}</body></html>`);
 });
-
-router.get('/hello/:name',(req,res)=>{
-	res.send(`Hello, ${req.pathParams.name}`);
-})
-.pathParam('name', joi.string().required(), 'Name to greet')
-.response(['text/plain'], 'A personalized greeting')
-.summary('Personalized greeting')
-.description('Prints a personalized greeting')
 
 
