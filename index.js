c1f8b0c833d590f1fbe23223fa64f3d46c600092
Daniel <dagr_spam@freenet.de>
2020-06-18 08:50:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ router.get('/show-log',(req,res)=>{
 router.put('/write-log',(req,res)=>{
 	var { query } = require("@arangodb");
 	var max = 13;
-	var s=JSON.stringify(req.body.data);
+	var s=req.body.data;
 	var log = query`
 	  INSERT 
 	  {
